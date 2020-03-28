14fb8e2092081fd5d6b3821c52ccc1c48dd6f205
Olufemi Oye <olufemioye@Olufemis-MacBook-Pro.local>
2020-03-28 23:22:03 +0100

    router and routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: () => import ('@/components/pages/Home.vue')
+  },
+  {
+    path: '/resume',
+    component: () => import ('@/components/pages/Resume.vue')
+  },
+  {
+    path: '/person',
+    component: () => import ('@/components/pages/Person.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
