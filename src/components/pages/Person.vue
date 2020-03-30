f7de71c2a58b08b972d5cc3a4b1569ad72a55954
Olufemi Oye <olufemioye@Olufemis-MacBook-Pro.local>
2020-03-30 09:02:37 +0100

    instagram feed

--- src/components/pages/Person.vue
+++ src/components/pages/Person.vue
@@ -1,5 +1,5 @@
 <template>
-	<main class="layout-main">
+	<div class="page page--person">
 		<div class="layout-container">
 			<div class="page-content">
 				<h1 class="heading">Person</h1>
@@ -16,16 +16,23 @@
 					I really like building things, I like watching things grow from ground up. Currently I am using my knowledge in design and code to build better and meaniful digital experiences. I have an interest in building businesses at scale.
 				</p>
 			</div>
-
 		</div>
 		<div class="page-visual">
-			
+			<p class="caption text-silver">Latest from my Instagram</p>
+			<div class="layout-container">
+				<InstagramFeed />
+			</div>
 		</div>
-	</main>
+	</div>
 </template>
 
 <script>
+	import InstagramFeed from '@/components/snippets/InstagramFeed.vue';
+
 	export default {
-		name: 'Person'
+		name: 'Person',
+		components: {
+			InstagramFeed
+		}
 	}
 </script>
